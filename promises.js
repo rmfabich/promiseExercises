448d58636da531fa9718873ed6e1ac86b28a5704
Rebecca Fabich <36006430+rmfabich@users.noreply.github.com>
2018-02-15 12:51:28 -0700

    Update promises.js

--- promises.js
+++ promises.js
@@ -5,21 +5,21 @@
 
 //here's what they look like :
 
-var promise = new Promise((resolve, reject)=>{
+var stuff = new Promise((resolve, reject)=>{
 	console.log('running some code inside promise');
-	resolve();
+	resolve(); //resolve is used the same as callback. Does not matter what you call it.
 
 });
 
-console.log(promise);
+//console.log(promise);
 
 //execute the file to see the output - notice that its just a 
 //dang promise object. A promise doesn't do anything until 
 //tell it to. Essentially, you run a promise by using the
 //"then" method on the promise object
-//comment the console log above, and uncomment out the code below
+//comment the console log above, and comment out the code below
 //to see how you use the then method 
 
-// promise.then(()=>{
-// 	console.log('running code after promise');
-// });
+ stuff.then(()=>{   //stuff.then(function()){     this is the same thing.
+ 	console.log('running code after promise');
+ });
