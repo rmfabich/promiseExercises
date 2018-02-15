661cf31188a756d6f793cc3bf7e5cbbe8707ac46
[Rebecca Fabich] <[rmfabich@gmail.com]>
2018-02-15 12:48:08 -0700

    fisrt commit

--- promises9.js
+++ promises9.js
@@ -1,6 +1,7 @@
-//one of the best parts of chaining multiple promises together using Promise.all is that error handling only needs to be done once (so much better than callbacks).
-//If I reject any of these promises, then my error handler function (the second arg in Promise.all) gets called. Test it out by rejecting promise1, promise2, or
-//promise3 and see that you can catch it one place
+//so now we've gone over a lot of the syntax, but we haven't actually used them in an async function, 
+//which is really why they exist. So let's see how that looks:
+
+//let's mock an async event with a timeout
 
 let promise1 = new Promise((resolve, reject)=>{
         setTimeout(()=>{
@@ -28,3 +29,6 @@ Promise.all(allPromises).then((values)=>{
 	console.log(error)
 });
 
+//i've tried to use the data in a synchronous fashion, but the crown prince is undefined! (run the code to see) Oh no, let's fix that. 
+//use the promise to console log the crown prince (don't actually modify the promise variable itself, use then() to
+//extract the data
